2aac5d088f29ed423ddbf9027db146de20ef15e6
devnior69 <devnior69@gmail.com>
2021-05-19 18:03:40 +0200

    Remove unused links

--- src/components/Modal/About.vue
+++ src/components/Modal/About.vue
@@ -5,13 +5,13 @@
       <a href="https://twitter.com/fundifinance" target="_blank">
         <Icon size="32" name="twitter" class="mr-1 mx-2" />
       </a>
-      <a href="https://discord.gg/yq8wfHd" target="_blank">
+      <a href="https://discord.gg/7kuuurrD" target="_blank">
         <Icon size="32" name="discord" class="mr-1 mx-2" />
       </a>
       <a href="https://t.me/fundifinance" target="_blank">
         <Icon size="32" name="telegram" class="mr-1 mx-2" />
       </a>
-      <a :href="`https://github.com/fundifinance/fundi-vote`" target="_blank">
+      <a :href="`https://github.com/fundifinance`" target="_blank">
         <Icon size="32" name="github" class="mr-1 mx-2" />
       </a>
     </div>
@@ -29,23 +29,8 @@
         Binance Smart Chain
       </div>
       <div class="d-flex">
-        <span v-text="'Block number'" class="flex-auto text-gray mr-1" />
-        <a
-          :href="_bscscanLink(web3.blockNumber, 'block')"
-          target="_blank"
-          class="float-right"
-        >
-          {{ $n(web3.blockNumber) }}
-          <Icon name="external-link" class="ml-1" />
-        </a>
-      </div>
-      <div class="d-flex">
         <span v-text="'IPFS server'" class="flex-auto text-gray mr-1" />
         {{ ipfsNode }}
-      </div>
-      <div class="d-flex">
-        <span v-text="'Hub'" class="flex-auto text-gray mr-1" />
-        {{ hubUrl }}
       </div>
     </div>
   </UiModal>
